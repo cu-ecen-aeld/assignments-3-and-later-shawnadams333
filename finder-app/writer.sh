c86af43ae6d6ec5422aeefbb5267e4f45ed5a6ee
Shawn Adams <shawnadams333@gmail.com>
2025-04-22 20:20:54 -0600

    add writer.sh

---
+++ finder-app/writer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+usage_and_exit() {
+    echo "USAGE: $0 <file-to-write> <string-to-write>"
+    exit 1
+}
+
+if [[ $# -lt 2 ]]; then
+    echo "you missed at least one argument"
+    usage_and_exit
+fi
+
+writefile=$1
+writestr=$2
+
+echo $writestr > $writefile
+
+if [[ ! -e $writefile ]]; then
+    echo "error writing $writestr to $writefile"
+    exit 1
+fi
+
+exit 0

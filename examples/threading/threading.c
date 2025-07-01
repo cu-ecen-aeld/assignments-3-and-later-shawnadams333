78e7afb82cb47afc6c6592c0483710a61d4db4cb
Shawn Adams <shawnadams333@gmail.com>
2025-06-30 19:55:54 -0600

    assignment 4 part 1 - checkin initial threading code

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,29 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct thread_data *thread_func_args = (struct thread_data *) thread_param;
+
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+
+    if (rc != 0)
+    {
+        ERROR_LOG("pthread_mutex_lock() error: %d", rc);
+    }
+
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+
+    if (rc != 0)
+    {
+        ERROR_LOG("pthread_mutex_unlock() error: %d", rc);
+    }
+
+    thread_func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +50,33 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    bool retval = true;
+
+    // allocate a thread_data object
+    void *pData = malloc(sizeof(struct thread_data));
+
+    if (pData == NULL)
+    {
+        ERROR_LOG("malloc() error");
+        retval = false;
+    }
+    else
+    {
+        struct thread_data *thrData = (struct thread_data *)pData;
+        thrData->wait_to_obtain_ms = wait_to_obtain_ms;
+        thrData->wait_to_release_ms = wait_to_release_ms;
+        thrData->mutex = mutex;
+        thrData->thread_complete_success = false;
+
+        int rc = pthread_create(thread, NULL, threadfunc, pData);
+
+        if (rc != 0)
+        {
+            ERROR_LOG("pthread_create() error: %d", rc);
+            retval = false;
+        }
+    }
+
+    return retval;
 }
 
